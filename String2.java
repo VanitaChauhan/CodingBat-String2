174267e9b6aa66a7427d4a9e521b789a4b54ef61
VanitaChauhan <vanitachauhan0@gmail.com>
2021-08-07 17:37:11 -0700

    Update String2.java

--- String2.java
+++ String2.java
@@ -1,4 +1,6 @@
-##Return true if the string "cat" and "dog" appear the same number of times in the given string.
+
+/*doubleChar*/
+/*Return true if the string "cat" and "dog" appear the same number of times in the given string*/
 
 
 catDog("catdog") → true
@@ -23,8 +25,8 @@ public boolean catDog(String str) {
 }
 
 
-
-##Return the number of times that the string "code"appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+/*countCode*/
+/*Return the number of times that the string "code"appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count*/
 
 
 public int countCode(String str) {
@@ -38,11 +40,12 @@ public int countCode(String str) {
 }
 
 
-##Given two strings, return true if either of the
+/*endOther*/
+/*Given two strings, return true if either of the
  strings appears at the very end of the other string
  , ignoring upper/lower case differences 
  (in other words, the computation should not be 
- "case sensitive").
+ "case sensitive")*/
 
 
  public boolean endOther(String a, String b) {
@@ -54,7 +57,9 @@ public int countCode(String str) {
 }
 
 
-/*Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not. */
+/*xyzThere*/
+/*Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). 
+So "xxyz" counts but "x.xyz" does not. */
 
 public boolean xyzThere(String str) {
   for(int i=0; i< str.length()-2; i++){
@@ -70,7 +75,11 @@ public boolean xyzThere(String str) {
   return false;
 }
 
-##Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+/*bobThere*/
+/*Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char*/
+
+
 public boolean bobThere(String str) {
   for(int i=0; i<str.length()-2; i++){
     if(str.charAt(i)=='b'&& str.charAt(i+2)=='b')
@@ -78,7 +87,13 @@ public boolean bobThere(String str) {
     }return false;
   } 
 
-##We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+
+/*xyBalance*/
+/*We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string. 
+    So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.*/
+        
+        
+        
 public boolean xyBalance(String str) {
   boolean y = false;
   
@@ -94,7 +109,11 @@ public boolean xyBalance(String str) {
 }
 
 
-Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+/*mixString*/
+/*Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second char of a, the second char of b, and so on.
+    Any leftover chars go at the end of the result.*/
+
+
 public String mixString(String a, String b) {
   String result = "";
   int i;
@@ -110,12 +129,13 @@ public String mixString(String a, String b) {
   return result;
 }
 
-#Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
 
-
+/*repeatEnd*/
+/*Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
+You may assume that n is between 0 and the length of the string, inclusive.
 repeatEnd("Hello", 3) → "llollollo"
 repeatEnd("Hello", 2) → "lolo"
-repeatEnd("Hello", 1) → "o"
+repeatEnd("Hello", 1) → "o"*/
 
 
 public String repeatEnd(String str, int n) {
@@ -130,12 +150,13 @@ public String repeatEnd(String str, int n) {
 }
 
 
-##Given a string and an int n, return a string made of the first n characters of the string, followed by the first n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string, inclusive (i.e. n >= 0 and n <= str.length()).
-
-
+/*repeatFront*/
+/*Given a string and an int n, return a string made of the first n characters of the string, 
+followed by the first n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string,
+inclusive (i.e. n >= 0 and n <= str.length()).
 repeatFront("Chocolate", 4) → "ChocChoChC"
 repeatFront("Chocolate", 3) → "ChoChC"
-repeatFront("Ice Cream", 2) → "IcI"
+repeatFront("Ice Cream", 2) → "IcI" */
 
 
 public String repeatFront(String str, int n) {
@@ -148,12 +169,11 @@ public String repeatFront(String str, int n) {
 }
 
 
-##Given two strings, word and a separator sep, return a big string made of count occurrences of the word, separated by the separator string.
-
-
+/*repeatSeparator*/
+/*Given two strings, word and a separator sep, return a big string made of count occurrences of the word, separated by the separator string.
 repeatSeparator("Word", "X", 3) → "WordXWordXWord"
 repeatSeparator("This", "And", 2) → "ThisAndThis"
-repeatSeparator("This", "And", 1) → "This"
+repeatSeparator("This", "And", 1) → "This"*/
 
 
 public String repeatSeparator(String word, String sep, int count) {
@@ -169,12 +189,13 @@ public String repeatSeparator(String word, String sep, int count) {
 
 
 
-##Given a string, consider the prefix string made of the first N chars of the string. Does that prefix string appear somewhere else in the string? Assume that the string is not empty and that N is in the range 1..str.length().
-
-
+/*prefixAgain*/
+/*Given a string, consider the prefix string made of the first N chars of the string. Does that prefix string appear somewhere else in the string?
+Assume that the string is not empty and that N is in the range 1..str.length().
 prefixAgain("abXYabc", 1) → true
 prefixAgain("abXYabc", 2) → true
-prefixAgain("abXYabc", 3) → false
+prefixAgain("abXYabc", 3) → false*/
+
 
 public boolean prefixAgain(String str, int n) {
   String prefix = str.substring(0, n);
@@ -187,13 +208,14 @@ public boolean prefixAgain(String str, int n) {
 }
 
 
-##
-A sandwich is two pieces of bread with something in between. Return the string that is between the first and last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of bread.
-
-
+/*getSandwich*/
+/*A sandwich is two pieces of bread with something in between.
+Return the string that is between the first and last appearance of "bread" in the given string,
+or return the empty string "" if there are not two pieces of bread.
 getSandwich("breadjambread") → "jam"
 getSandwich("xxbreadjambreadyy") → "jam"
-getSandwich("xxbreadyy") → ""
+getSandwich("xxbreadyy") → "" */
+
 
 public String getSandwich(String str) {
   int first = -1;
@@ -218,13 +240,14 @@ public String getSandwich(String str) {
 }
 
 
-##
-Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
 
-
+/*sameStarChar*/
+/*Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
 sameStarChar("xy*yzz") → true
 sameStarChar("xy*zzz") → false
-sameStarChar("*xa*az") → true
+sameStarChar("*xa*az") → true*/
+
+
 
 public boolean sameStarChar(String str) {
   for(int i = 1; i < str.length() - 1; i++) {
@@ -235,12 +258,14 @@ public boolean sameStarChar(String str) {
   return true;
 }
 
-##Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
 
-
+/*oneTwo*/
+/*Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca". 
+Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
 oneTwo("abc") → "bca"
 oneTwo("tca") → "cat"
-oneTwo("tcagdo") → "catdog"
+oneTwo("tcagdo") → "catdog" */
+
 
 public String oneTwo(String str) {
   String result = "";
@@ -253,13 +278,14 @@ public String oneTwo(String str) {
 }
 
 
-##
-Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
 
 
+/*zipZap*/
+/*Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'.
+Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
 zipZap("zipXzap") → "zpXzp"
 zipZap("zopzop") → "zpzp"
-zipZap("zzzopzop") → "zzzpzp"
+zipZap("zzzopzop") → "zzzpzp" */
 
 
 
@@ -271,12 +297,14 @@ public String zipZap(String str) {
   
 }
 
-##Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
 
-
+/*starOut*/
+/*Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone.
+So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
 starOut("ab*cd") → "ad"
 starOut("ab**cd") → "ad"
-starOut("sm*eilly") → "silly"
+starOut("sm*eilly") → "silly"*/
+
 
 public String starOut(String str) {
   int len = str.length();
@@ -294,12 +322,14 @@ public String starOut(String str) {
   return result;
 }
 
-##Given a string and a non-empty word string, return a version of the original String where all chars have been replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.
 
-
+/*plusOut*/
+/*Given a string and a non-empty word string, return a version of the original String where all chars have been replaced by pluses ("+"), 
+except for appearances of the word string which are preserved unchanged.
 plusOut("12xy34", "xy") → "++xy++"
 plusOut("12xy34", "1") → "1+++++"
-plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"
+plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"*/
+
 
 public String plusOut(String str, String word) {
   int slen = str.length();
@@ -326,13 +356,14 @@ public String plusOut(String str, String word) {
 
 
 
-
-Given a string and a non-empty word string, return a string made of each char just before and just after every appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
-
-
+/*wordEnds*/
+/*Given a string and a non-empty word string, return a string made of each char just before and just after every 
+appearance of the word in the string. Ignore cases where there is no char before or after the word, 
+and a char may be included twice if it is between two words.
 wordEnds("abcXY123XYijk", "XY") → "c13i"
 wordEnds("XY123XY", "XY") → "13"
-wordEnds("XY1XY", "XY") → "11"
+wordEnds("XY1XY", "XY") → "11" */
+
 
 public String wordEnds(String str, String word) {
   String result = "";
